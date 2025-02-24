90d147c5ffdfc1765f45f01a811421f4e92f1e76
leonardo-luz <leonardoluz.f02@gmail.com>
2025-02-24 13:38:36 -0300

    feat: add basic plugin structure and functionality

---
+++ README.md
@@ -0,0 +1,58 @@
+# http.nvim
+
+*A Neovim plugin providing a simple function for making HTTP requests using curl.*
+
+## Features
+
+* **`fetch` function:**  Performs HTTP requests and returns the response data as a Lua table.
+* **Methods:** Supports all HTTP methods, including `GET`, `POST`, `PUT`, `DELETE`, and `PATCH` requests.
+
+## Dependencies
+
+* `curl` (must be installed on your system)
+
+## Installation
+
+Add `leonardo-luz/http.nvim` to your Neovim plugin manager.  For example, in your `init.lua` (or equivalent):
+
+```lua
+{
+  'leonardo-luz/http.nvim',
+}
+```
+
+## Usage
+
+Require the plugin using either:
+
+* `local http = require('http')`
+* `local fetch = require('http').fetch`
+
+## Example
+
+```lua
+local http = require('http')
+
+local function example()
+  local url = "https://example.com/users"
+  local opts = {
+    method = "POST", -- default: GET
+    headers = {
+		"Content-Type: application/json",
+    },
+    body = {
+		username = "test",
+    },
+  }
+  local response = http.fetch(url, opts)
+
+  -- Process the response (response is a table)
+  print(response)
+end
+
+example()
+```
+
+## License
+
+This project is licensed under the [MIT License](LICENSE.md).
---
+++ lua/http.lua
@@ -0,0 +1,69 @@
+local M = {}
+
+---Parse headers
+---@param headers string[]
+---@return string
+local parse_headers = function(headers)
+	local parsed = ""
+	for _, value in ipairs(headers) do
+		parsed = parsed .. " -H '" .. value .. "'"
+	end
+
+	return parsed
+end
+
+---HTTP request
+---
+---Usage example:
+---local http = require('http')
+---local response = http.fetch("https://example.com/users", {
+---	method = "POST",
+---	headers = {
+---		"Content-Type: application/json",
+---	},
+---	body = {
+---		username = "test",
+---	},
+---})
+---@param url string
+---@param opts { method: string, headers: string[], body: table }|nil
+---@return { response: table|nil, err: string|nil }
+M.fetch = function(url, opts)
+	local method = "-X GET"
+	local headers = ""
+	local body = ""
+
+	if opts then
+		method = opts.method and " -X " .. opts.method or " -X GET"
+		headers = opts.headers and parse_headers(opts.headers) or ""
+		body = opts.body and " --data '" .. vim.json.encode(opts.body, { escape_slash = true }) .. "'" or ""
+	end
+
+	local request = "curl -s --max-time 60 " .. url .. method .. headers .. body
+
+	local handle = io.popen(request)
+	if handle == nil then
+		print("Error: Could not execute http request.")
+		return { err = "http request failed" }
+	end
+
+	local data, err = handle:read("*a")
+	handle:close()
+
+	if err then
+		print("Error reading http response: " .. err)
+		return { err = "response read failed" }
+	end
+
+	local decoded_data = vim.json.decode(data)
+	if not decoded_data then
+		print("Error decoding JSON response. " .. "\nResponse: " .. data)
+		return { err = "JSON decoding failed" }
+	end
+
+	return { response = decoded_data }
+end
+
+M.setup = function(opts) end
+
+return M
